1b646fe29c0c9b67fe1bc1b51e12c3cec9aec001
Xinran Li <xinranli5982@gmail.com>
2024-10-21 21:36:19 -0400

    divide into shorter videos

--- P3/p5_project/sketch.js
+++ P3/p5_project/sketch.js
@@ -2,18 +2,27 @@ let vid;
 let playing = true;
 let cloudDensity = 0.5;
 let circleDiameter; 
+let noiseOffsetX = 0;  
+let noiseOffsetY = 0; 
+
+// use skyState to decide which video to play!
+
+// NIGHT -- all photoresistors dark --> skyState = 0, night.mp4
+// MORNING -- photoresistors on the east side brighter than the west side --> sometime in the morning
+  // depending on the brightness, can be beforesunrise(darkest), sunrise, morning(brightest)
+// NOON -- all photoresisters equally bright! --> noon
+// AFTERNOON --photoresistors on the west side brighter then the east side --> sometime in the afternoon
+  // depending on the brightness, can be aftersunset(darkest), sunset, afternoon(brightest)
+
+let skyState = 2;
+let videoFiles = ['night.mp4', 'beforesunrise.mp4', 'sunrise.mp4', 'morning.mp4', 'noon.mp4', 'afternoon.mp4', 'sunset.mp4', 'aftersunset.mp4'];
 
 function setup() {
   createCanvas(windowWidth, windowHeight); 
   background(0); 
   console.log("Canvas size (width x height):", width, "x", height);
   circleDiameter = min(width, height) / 2;
-
-  vid = createVideo('square.mp4');
-  vid.size(circleDiameter, circleDiameter); 
-  vid.volume(0);
-  vid.loop();
-  vid.hide();
+  loadVideoForSkyState();
 }
 
 function draw() {
@@ -23,31 +32,9 @@ function draw() {
   let yCenter = (height - circleDiameter) / 2;
   image(img, xCenter, yCenter, circleDiameter, circleDiameter); 
   drawClouds(xCenter, yCenter);
-}
 
-function drawSmoothClouds(xCenter, yCenter) {
-  let cloudRadius = circleDiameter / 2;
-
-  loadPixels(); 
-
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      let distFromCenter = dist(x, y, xCenter + cloudRadius, yCenter + cloudRadius);
-      
-      if (distFromCenter < cloudRadius) {
-        let noiseValue = noise(x * 0.01, y * 0.01); 
-        let alphaValue = map(noiseValue, 0, 1, 0, 255); 
-
-        let pixelIndex = (x + y * width) * 4;
-        pixels[pixelIndex] = 255; 
-        pixels[pixelIndex + 1] = 255; 
-        pixels[pixelIndex + 2] = 255; 
-        pixels[pixelIndex + 3] = alphaValue * cloudDensity; 
-      }
-    }
-  }
-
-  updatePixels(); 
+  noiseOffsetX += random (-0.002, 0.005);
+  noiseOffsetY += random (-0.002, 0.005);
 }
 
 function drawClouds(xCenter, yCenter) {
@@ -55,22 +42,37 @@ function drawClouds(xCenter, yCenter) {
   noStroke();
 
   let cloudRadius = circleDiameter / 2;
-  let pixelSize = circleDiameter / 300; 
+  let pixelSize = circleDiameter / 200; 
 
   for (let i = 0; i < 500; i++) { 
     for (let j = 0; j < 500; j++) { 
-      let ran = noise(i / 50, j / 50); 
+      let ran = noise(i / 50 + noiseOffsetX, j / 50 + noiseOffsetY); 
       let cloudColor = map(ran, 0, 1, 0, 255); 
       
       let x = map(i, 0, 200, -cloudRadius, cloudRadius);
       let y = map(j, 0, 200, -cloudRadius, cloudRadius);
       
       if (dist(x, y, 0, 0) < cloudRadius) {
-        fill(255, 255, 255, cloudColor * cloudDensity); 
+        if (skyState == 0) {
+          fill(0, 0, 0, cloudColor * cloudDensity); 
+        } else {
+          fill(255, 255, 255, cloudColor * cloudDensity); 
+        }
         rect(xCenter + cloudRadius + x, yCenter + cloudRadius + y, pixelSize, pixelSize); 
       }
     }
   }
+}
+
+function loadVideoForSkyState() {
+  if (vid) {
+    vid.remove();
+  }
+  vid = createVideo(videoFiles[skyState]);
+  vid.size(circleDiameter, circleDiameter); 
+  vid.volume(0);
+  vid.loop();
+  vid.hide();
 }
 
 function mousePressed() {
